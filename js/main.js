71e6f29656159a3f8650044e68378eece98a8fc1
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-02-05 13:10:31 -0600

    scale 1 1.5 1 ligth 3---enrico iphone v12 trasp

--- js/main.js
+++ js/main.js
@@ -6,9 +6,22 @@ window.addEventListener('camera-error', (error) => {
     console.log('camera-error', error);
 })
 
-window.addEventListener('model-loaded', ()=>{
+window.addEventListener('model-loaded', (e)=>{
     console.log("Modelo listo XD");
     document.getElementById("splassScreen").style.display="none";
+    console.log(e.detail.model)
+    let azuldelgado = e.detail.model.getObjectByName('AzulDelgado001')
+    let AzulGrueso001 = e.detail.model.getObjectByName('AzulGrueso001')
+    let BlancoGrueso001 = e.detail.model.getObjectByName('BlancoGrueso001')
+    let BlancoTop001 = e.detail.model.getObjectByName('BlancoTop001')
+    let TelaTempur001 = e.detail.model.getObjectByName('TelaTempur001')
+    AzulGrueso001.material.opacity=0.1
+    azuldelgado.material.opacity=0.1
+    BlancoGrueso001.material.opacity=0.1
+    BlancoTop001.material.opacity=0.1
+    TelaTempur001.material.opacity=0.1
+    // e.detail.model.getObjectByName('AzulDelgado');
+    
 });
 
 AFRAME.registerComponent('registerevents', {
